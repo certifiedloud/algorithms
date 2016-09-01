8875957573d4548b4a8a22d4d61866cc442c42ad
Cody Heimberger <rcodyheimberger@gmail.com>
2016-09-01 07:07:33 -0600

    there's a bug somewhere in here, but it's getting close

--- mergeSort/mergeSort.py
+++ mergeSort/mergeSort.py
@@ -2,30 +2,39 @@
 from random import randint
 
 def merge_sort(my_array):
-    left = my_array[:round(len(my_array)/2)]
-    right = my_array[round(len(my_array)/2):]
-    print(left, right, my_array)
+    if(len(my_array) < 2):
+        return my_array
 
-    if(len(left) < 2 or len(right) < 2):
-        return merge(left, right)
+    else:
+        left = my_array[:round(len(my_array)/2)]
+        right = my_array[round(len(my_array)/2):]
 
-    return merge_sort(left)
-    return merge_sort(right)
+        a = merge_sort(left)
+        b = merge_sort(right)
+        return merge(a,b)
+
 
 def merge(left, right):
-    #merge the two sorted arrays
+    #merge the two arrays
     i=0
     j=0
     combined = []
-    for k in range(len(left) + len(right)):
+    combinedLength = len(left) + len(right)
+    for k in range(combinedLength):
         if left[i] < right[j]:
             combined.insert(k, left[i])
+            if combinedLength == 2:
+                combined.insert(k+1, right[j])
+                break
             ++i
-        elif right[j] < left[i]:
+        else:
             combined.insert(k, right[j])
+            if combinedLength == 2:
+                combined.insert(k+1, left[i])
+                break
             ++j
+    print(combined)
     return combined
-
 
 my_array = [4,3,6,5,1,2,8,7]
 
