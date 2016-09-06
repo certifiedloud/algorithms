846e5608aeb803ee406b9db1808f6aa1e0d3734f
Cody Heimberger <rcodyheimberger@gmail.com>
2016-09-06 06:35:22 -0600

    start inversion counter algorithm

---
+++ countInversions/countInversions.py
@@ -0,0 +1,43 @@
+#An implementation of the merge sort algorithm
+from random import randint
+
+def merge_sort(my_array):
+    if(len(my_array) < 2):
+        return my_array
+
+    else:
+        left = my_array[:round(len(my_array)/2)]
+        right = my_array[round(len(my_array)/2):]
+
+        a = merge_sort(left)
+        b = merge_sort(right)
+        return merge(a,b)
+
+
+def merge(left, right):
+    #merge the two arrays
+    i=0
+    j=0
+    combined = []
+    combinedLength = len(left) + len(right)
+    for k in range(combinedLength):
+        if left[i] < right[j]:
+            combined.insert(k, left[i])
+            if combinedLength == 2:
+                combined.insert(k+1, right[j])
+                break
+            ++i
+        else:
+            combined.insert(k, right[j])
+            if combinedLength == 2:
+                combined.insert(k+1, left[i])
+                break
+            ++j
+    print(combined)
+    return combined
+
+my_array = [4,3,6,5,1,2,8,7]
+
+sorted_array = merge_sort(my_array)
+
+print("RESULT: ", sorted_array)
