b51421eed42a0e21aa692316fb271f37db9e7ce6
Cody Heimberger <certifiedloud@gmail.com>
2016-09-06 13:35:47 -0600

    keep running total of inversions and return final count

--- countInversions/countInversions.py
+++ countInversions/countInversions.py
@@ -8,16 +8,17 @@ def merge_sort_count(my_array):
         left = my_array[:round(len(my_array)/2)]
         right = my_array[round(len(my_array)/2):]
 
-        left_count, a = merge_sort_count(left)
-        right_count, b = merge_sort_count(right)
-        return merge(a,b)
+        a, left_count = merge_sort_count(left)
+        b, right_count = merge_sort_count(right)
+        sorted_array, inv_count =  merge(a,b)
+        return sorted_array, inv_count + left_count + right_count
 
 
 def merge(left, right):
-    #merge the two arrays
-    #TODO refactor the return the count of inversions
+    #merge the two arrays and count inversions
     i=0
     j=0
+    inversion_count = 0
     combined = []
 
     while i < len(left) and j < len(right):
@@ -25,8 +26,11 @@ def merge(left, right):
             combined.append(left[i])
             i+=1
         else:
+            #increment the amount of split inversion found
+            inversion_count += len(left) - i
             combined.append(right[j])
             j+=1
+
 
     while i < len(left):
         combined.append(left[i])
@@ -36,11 +40,11 @@ def merge(left, right):
         combined.append(right[j])
         j += 1
 
-    return combined
+    return combined, inversion_count
 
 #this array has 3 inversions
-my_array = [1,3,5,2,4,6]
+my_array = [1,3,5,2,4,6] # 3
 print("sorting and counting inversions")
-inv_count = merge_sort_count(my_array)
+sorted_array, inv_count = merge_sort_count(my_array)
 
-print("This array should have 3 inversions, we found: ", inv_count)
+print("Found inversions ", inv_count)
