d68e42e2a41ff43c2e3ba015898ad9e170144537
Cody Heimberger <rcodyheimberger@gmail.com>
2016-09-10 20:12:47 -0600

    still doesn't work quite right

--- quickSort/quickSort.py
+++ quickSort/quickSort.py
@@ -1,34 +1,30 @@
 #An implementation of the QuickSort algorithm
 
-def choose_pivot(array):
-    #naively choose the first element in the array
-    return array[0]
-
 def partition(array, l, r):
-    p = choose_pivot(array)
+    print("partitioning ", array)
+    p = array[l]
     i = l + 1
     for j in range(l+1, r):
         if array[j] < p:
             array[i], array[j] = array[j], array[i]
             i = i+1
     array[l], array[i-1] = array[i-1], array[l]
+    print("partitioned ", array, p)
+    print(array[:p], array[p+1:])
+    return i
 
-def quick_sort(starting_array):
-    if len(starting_array) == 1:
-        return starting_array
+def quick_sort(array, l, r):
+    if l < r:
+        pivot = partition(array, l, r)
 
-
-    partition(starting_array, 0, len(starting_array)-1)
-
-    # quick_sort(starting_array[:pivot_index - 1])
-    # quick_sort(starting_array[pivot_index + 1:])
-
-    return starting_array
+        quick_sort(array, l, pivot - 1)
+        quick_sort(array, pivot + 1, r)
 
 def main():
-    starting_array = [3,8,2,5,1,4,7,6]
-    sorted_array = quick_sort(starting_array)
-    print(sorted_array)
+    array = [3,8,1,5,2,4,7,6]
+    # starting_array = [1,2,3,4,5,6,7,8]
+    quick_sort(array, 0, len(array)-1)
+    print(array)
 
 if __name__ == '__main__':
     main()
