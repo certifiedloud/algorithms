f8ca5b47d4ba33483804f5c80cee18049b5cd166
Cody Heimberger <rcodyheimberger@gmail.com>
2016-09-19 21:23:11 -0600

    get data read in properly

--- karger/karger.py
+++ karger/karger.py
@@ -1,1 +1,27 @@
 # Karger's algorithm for finding min cuts of graphs
+import random
+import csv
+
+def read_data():
+    # read each row as a list, then store this list in a list of lists
+    graph = []
+    f = open('graph.txt')
+    csv_f = csv.reader(f, delimiter='\t')
+    for row in csv_f:
+      graph.append(row[1:len(row)-1])
+    return graph
+
+
+def karger():
+    # contract two random points
+
+    # delete self-loops
+    pass
+
+def main():
+    graph = read_data()
+    print(graph[199])
+    karger()
+
+if __name__ == '__main__':
+    main()
