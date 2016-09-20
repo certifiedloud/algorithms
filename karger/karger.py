aeccf3a61b2ea075c3f0036df1823e6db2ddfe30
Cody Heimberger <rcodyheimberger@gmail.com>
2016-09-19 21:46:43 -0600

    tweak incoming data

--- karger/karger.py
+++ karger/karger.py
@@ -8,7 +8,14 @@ def read_data():
     f = open('graph.txt')
     csv_f = csv.reader(f, delimiter='\t')
     for row in csv_f:
-      graph.append(row[1:len(row)-1])
+      graph.append(row)
+
+    for sub_list in graph:
+        for i in range(len(sub_list)):
+            try:
+                sub_list[i] = int(sub_list[i])
+            except:
+                sub_list.pop(i)
     return graph
 
 
@@ -20,7 +27,7 @@ def karger():
 
 def main():
     graph = read_data()
-    print(graph[199])
+    print(graph)
     karger()
 
 if __name__ == '__main__':
